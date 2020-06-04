d9e44b7bf764e55bf3c2f847ff2e4060e2331ad6
S-Delowar <sayed.buet97@gmail.com>
2020-06-04 22:42:47 +0600

    houses api done

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config()
+const MongoClient = require('mongodb').MongoClient;
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+
+const port = process.env.PORT;
+const uri = process.env.DB_PATH;
+//console.log(uri)
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my api');
+})
+
+
+app.post('/addHouse', (req, res) => {
+    const product = req.body;
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("AirCNC").collection("houses");
+        collection.insert(product, (err, result)=>{
+            if(err){
+                console.log(err)
+                res.status(500).send({message:err});
+            }
+            else{
+                res.send(result.ops[0]);
+            }
+        });
+        //client.close();
+      });
+});
+
+app.get('/houses', (req, res) =>{
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("AirCNC").collection("houses");
+        collection.find().toArray((err, documents)=>{
+            if(err){
+                console.log(err)
+                res.status(500).send({message:err});
+            }
+            else{
+                res.send(documents);
+            }
+        });
+        client.close();
+      });
+});
+
+
+app.listen(port, () => {
+    console.log(`We are live on port ${port}`);
+})
